32dbf3d22a9193676cc55ac5dd5b77ba9048f0a2
smartlife-ao-boso <slmcboso.masaomiito@gmail.com>
2026-05-09 19:20:36 +0900

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,1 +1,1 @@
-window.BOSO_GAS_URL = "https://script.google.com/macros/s/AKfycbzBW0GuJtVFlbT6uUT9t6urmoYvbqWvB7quii3X88103___Zd0hQTI6HVRlbBB97Abq/exec";
+window.BOSO_GAS_URL = "https://script.google.com/macros/s/AKfycbxCbO2LXR7AAfkvwMS3aSz1lPOD447iKANCW7HW-luW_LFaoLu84nY7n2pMjwbxJ6GR/exec";
