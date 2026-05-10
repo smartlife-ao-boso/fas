2508697c2120d4957f1a526341fb8c6ea026b84d
smartlife-ao-boso <slmcboso.masaomiito@gmail.com>
2026-05-10 11:43:15 +0900

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -1,1 +1,1 @@
-window.BOSO_GAS_URL = "https://script.google.com/macros/s/AKfycbxXyhKdw7JFh1iycownJ2Ntf440gdloWSwBsryyNZoH1ogGFqOCq9ENDecHpd1WcS9A/exec";
+window.BOSO_GAS_URL = "https://script.google.com/macros/s/AKfycbxPlD1PA17V8Yr1HQw-EnBvgzxbat_REpI40BhdSQHQwfYCjFp8fFVTy_WGkn49K8Vg/exec";
