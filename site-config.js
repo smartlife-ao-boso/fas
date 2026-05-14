52dd64d2459f8e3f6821fc44d13c71d879cde67d
smartlife-ao-boso <slmcboso.masaomiito@gmail.com>
2026-05-14 16:10:41 +0900

    Update BOSO_GAS_URL to new endpoint

--- site-config.js
+++ site-config.js
@@ -1,1 +1,1 @@
-window.BOSO_GAS_URL = "https://script.google.com/macros/s/AKfycbxMi5hG57MdS1AJKee2Ief6otD5xUcbFb4FHjkTi8TDsGNtdUmN2xM77Mw-PMx7mIBf/exec";
+window.BOSO_GAS_URL = "https://script.google.com/macros/s/AKfycbwaFfQcoAl_tQBpjBDxJ0W2u8P5qEUXtGI1nfryQOiS3K2_rIzgAoxVli8tZLauk5vw/exec";
